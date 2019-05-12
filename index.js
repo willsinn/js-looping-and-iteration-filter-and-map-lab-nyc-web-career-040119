66726f6ef3f8efa21f7ce6655d07dea6d39deede
William Sinn <w.h.sinn@gmail.com>
2019-05-12 15:58:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here:
+drivers = [
+      { name: 'Sally',   revenue: 400 },
+      { name: 'Annette', revenue: 200 },
+      { name: 'Jim',     revenue: 150 }
+    ];
+function driversWithRevenueOver(driversArr, revenue) {
+    let aboveNumDrivers = driversArr.map(driver => isRevenueHigher(driver, revenue));
+    return aboveNumDrivers;
+}
+
+function isRevenueHigher(driver, num) {
+  let driverRevenue = driver["revenue"];
+  if (driverRevenue > num) {
+    return driver;
+  }
+  return;
+}
